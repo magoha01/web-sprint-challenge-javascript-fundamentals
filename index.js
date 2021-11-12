413e12706dacaba1f166d0205351b5ab8c00a0ca
Hailey Magoline <magoline.hailey@gmail.com>
2021-11-12 10:59:24 -0600

    finished callback section

--- index.js
+++ index.js
@@ -117,30 +117,39 @@ const zooAnimals = [
     * The consume function should return the invocation of cb, passing a and b into cb as arguments
   */
 
-  function consume(/*Your Code Here */){
-    /*Your Code Here */
-  }
- 
+
   
+
+  function consume(a, b, callBackcb){
+    const result = callBackcb(a,b);
+    return result;
+  }
+
+    function callBack(a, b){
+      return a + b;
+    }
+
+  // console.log(consume(1, 2, callBack))
+
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(a,b){
+   return a +b;
   }
 
 
 // 🦁🦁🦁 Use multiply to return the product of two numbers 🦁🦁🦁
   
-function multiply(/*Your Code Here */){
-   /*Your Code Here */
+function multiply(a,b){
+   return a * b;
   }
 
 
  // 🦁🦁🦁 Use greeting to accept a first and last name and return "Hello {first-name} {last-name}, nice to meet you!" 🦁🦁🦁
   
-function greeting(/*Your Code Here */){
-   return /*Your Code Here */
+function greeting(firstName, lastName){
+   return `Hello ${firstName} ${lastName}, nice to meet you!`
   }
   
   
@@ -155,8 +164,11 @@ function greeting(/*Your Code Here */){
 /* 🐴🐴🐴 Step 1: Base Constructor 🐴🐴🐴
  Use the constructor function named CuboidMaker to accept properties for length, width, and height which can be initialized as an object
 */
-function CuboidMaker(/*Your Code Here */){
-  /*Your Code Here */
+function CuboidMaker(/*code*/){
+
+      /*this.length = attrs.length;
+      this.width = attrs.width;
+      this.height = attrs.height;*/
 }
 
 
